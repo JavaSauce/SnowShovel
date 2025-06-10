421f07885375068cd905e97e9f0507871c6959c0
covers1624 <covers1624@gmail.com>
2025-06-10 22:52:08 +0930

    Don't extract a tool if its already extracted.

--- src/main/java/net/javasauce/ss/tasks/ToolTasks.java
+++ src/main/java/net/javasauce/ss/tasks/ToolTasks.java
@@ -1,6 +1,5 @@
 package net.javasauce.ss.tasks;
 
-import net.covers1624.quack.io.IOUtils;
 import net.covers1624.quack.maven.MavenNotation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +7,6 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 /**
  * Created by covers1624 on 1/20/25.
@@ -26,16 +22,13 @@ public class ToolTasks {
                     .resolve(notation.toModulePath())
                     .resolve(notation.version)
                     .resolve(notation.module + "-" + notation.version);
-            try (ZipInputStream zin = new ZipInputStream(Files.newInputStream(toolZip))) {
-                ZipEntry ent;
-                while ((ent = zin.getNextEntry()) != null) {
-                    if (ent.isDirectory()) continue;
-
-                    Files.copy(zin, IOUtils.makeParents(extractedDir.resolve(ent.getName())), StandardCopyOption.REPLACE_EXISTING);
-                }
+            if (Files.notExists(extractedDir)) {
+                ZipTasks.extractZip(toolZip, extractedDir);
+                LOGGER.info("Tool extracted.");
+            } else {
+                // TODO maybe validate somehow?
+                LOGGER.info("Output exists, Skipped.");
             }
-
-            LOGGER.info("Tool extracted.");
             return extractedDir.resolve(notation.withExtension("jar").toFileName());
         } catch (IOException ex) {
             throw new RuntimeException("Failed to extract tool.", ex);
